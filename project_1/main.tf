280ad3b6771533f196d186f8a2b213f7b688fb48
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 15:07:29 -0300

    feat: terraform mock code on project_1

---
+++ project_1/main.tf
@@ -0,0 +1,34 @@
+terraform {
+#  required_version = ">= 1.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.21.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = "false"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "test_nginx"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+resource "docker_container" "nginx2" {
+  image = docker_image.nginx.image_id
+  name  = "test_nginx_2"
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
